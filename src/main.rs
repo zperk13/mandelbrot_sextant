1e2b528e6bebc15e167efe5e5cf7a8503c94a086
zperk13 <46951331+zperk13@users.noreply.github.com>
2025-03-18 16:02:38 -0500

    added caching for pans

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,42 @@
 mod bits2d;
 mod sextant_terminal;
 
+use dashmap::DashMap;
 use rayon::prelude::*;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, atomic::{self, AtomicU64}};
 
 fn main() {
     let result = sextant_terminal::run(std::io::stdout(), None, on_event);
     result.unwrap();
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 struct Memory {
     scaler_x: Scaler,
     scaler_y: Scaler,
     threshhold: usize,
+    cache: DashMap<(HashableF64, HashableF64), bool>,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+struct HashableF64(f64);
+impl Eq for HashableF64 {}
+impl std::hash::Hash for HashableF64 {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.to_bits().hash(state);
+    }
+}
+impl From<f64> for HashableF64 {
+    fn from(value: f64) -> Self {
+        Self(value)
+    }
 }
 
 fn on_event(
     handler: &mut sextant_terminal::Handler<Option<Memory>>,
     event: Option<crossterm::event::KeyEvent>,
 ) -> bool {
+    let mut is_pan = false;
     if let Some(event) = event {
         use crossterm::event::KeyCode;
         let (additional_scaler, zoom_times, int_amount) = if event
@@ -35,18 +52,22 @@ fn on_event(
             (KeyCode::Char('w'), Some(memory)) => {
                 let amount = -memory.scaler_y.scalar * additional_scaler as f64;
                 memory.scaler_y.offset(amount);
+                is_pan = true;
             }
             (KeyCode::Char('s'), Some(memory)) => {
                 let amount = memory.scaler_y.scalar * additional_scaler as f64;
                 memory.scaler_y.offset(amount);
+                is_pan = true;
             }
             (KeyCode::Char('a'), Some(memory)) => {
                 let amount = -memory.scaler_x.scalar * additional_scaler as f64;
                 memory.scaler_x.offset(amount);
+                is_pan = true;
             }
             (KeyCode::Char('d'), Some(memory)) => {
                 let amount = memory.scaler_x.scalar * additional_scaler as f64;
                 memory.scaler_x.offset(amount);
+                is_pan = true;
             }
             (KeyCode::Char('='), Some(memory)) => {
                 for _ in 0..zoom_times {
@@ -61,7 +82,7 @@ fn on_event(
                 }
             }
             (KeyCode::Up, Some(memory)) => {
-                memory.threshhold+=int_amount;
+                memory.threshhold += int_amount;
             }
             (KeyCode::Down, Some(memory)) => {
                 memory.threshhold = memory.threshhold.saturating_sub(int_amount);
@@ -81,33 +102,60 @@ fn on_event(
             scaler_x,
             scaler_y,
             threshhold: 500,
+            cache: DashMap::new(),
         }
     });
     let Memory {
         scaler_x,
         scaler_y,
-        threshhold
+        threshhold,
+        cache,
     } = &memory;
     let bit_width = handler.bit_width();
     let bit_height = handler.bit_height();
     let arc_mutex = Arc::new(Mutex::new(&mut *handler));
+    let cache = Arc::new(cache);
+    let cache_hits = &AtomicU64::new(0);
     (0..bit_height).into_par_iter().for_each(move |py| {
         let y0 = scaler_y.scale(py as f64);
         for px in 0..bit_width {
             let x0 = scaler_x.scale(px as f64);
-            let mut x = 0.0;
-            let mut y = 0.0;
-            let mut x2 = 0.0;
-            let mut y2 = 0.0;
-            let mut iteration = 0;
-            while (x2 + y2 <= 4.0) && (iteration < *threshhold) {
-                y = (x + x) * y + y0;
-                x = x2 - y2 + x0;
-                x2 = x * x;
-                y2 = y * y;
-                iteration += 1;
-            }
-            let b = iteration == *threshhold;
+            let key = (HashableF64(x0), HashableF64(y0));
+            let calculate_b = || {
+                let mut x = 0.0;
+                let mut y = 0.0;
+                let mut x2 = 0.0;
+                let mut y2 = 0.0;
+                let mut iteration = 0;
+                while (x2 + y2 <= 4.0) && (iteration < *threshhold) {
+                    y = (x + x) * y + y0;
+                    x = x2 - y2 + x0;
+                    x2 = x * x;
+                    y2 = y * y;
+                    iteration += 1;
+                }
+                iteration == *threshhold
+            };
+            
+            // When zooming, the number of cache hits is usually 0 or 1,
+            // not worth spending time hashing for.
+            // However, there are MANY cache hits when panning.
+            let b = if is_pan {
+                match cache.get(&key) {
+                    Some(b) => {
+                        cache_hits.fetch_add(1, atomic::Ordering::Relaxed);
+                        *b.value()
+                    }
+                    None => {
+                        let b = calculate_b();
+                        cache.insert(key, b);
+                        b
+                    }
+                }
+            } else {
+                calculate_b()
+            };
+
             let mut lock = arc_mutex.lock().unwrap();
             lock.set_bit(px, py, !b);
         }
@@ -115,8 +163,9 @@ fn on_event(
     handler.render_bits().unwrap();
     handler
         .set_title(format!(
-            "Finished processing in {:?} threshhold={threshhold}",
+            "Finished processing in {:?} threshhold={threshhold} cache_hits={}",
             start.elapsed(),
+            cache_hits.load(atomic::Ordering::Relaxed)
         ))
         .unwrap();
     handler.memory = Some(memory);
@@ -173,8 +222,8 @@ impl Scaler {
         *self = Scaler::new(
             self.original_min,
             self.original_max,
-            self.target_min - (self.scalar/2.0),
-            self.target_max + (self.scalar/2.0),
+            self.target_min - (self.scalar / 2.0),
+            self.target_max + (self.scalar / 2.0),
         );
     }
 }
